03301aa35c5915a84e73da454b44534430380f1c
quockhanhcode <66151943+quockhanhcode@users.noreply.github.com>
2023-08-09 16:29:55 +0700

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Typography from "./components/Typography";
+import Field from "./components/Field";
+import Rate from "./components/Rate";
+import Drag from "./components/drag/Drag";
+import DridData from "./components/DridData";
+import Api from "./components/getapi/Api";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Typography />
+      <Field></Field>
+      <Rate></Rate> */}
+      {/* <Drag></Drag> */}
+      {/* <DridData></DridData> */}
+      <Api></Api>
+    </div>
+  );
+}
+
+export default App;
