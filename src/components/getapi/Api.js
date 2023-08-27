a8060f8e823e21696ac825c60c8e8a0f91e9fad9
quockhanhcode <66151943+quockhanhcode@users.noreply.github.com>
2023-08-27 15:12:41 +0700

    Update Api.js

--- src/components/getapi/Api.js
+++ src/components/getapi/Api.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { styled } from "@mui/material/styles";
-import data from "../../data.json";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
@@ -27,46 +26,85 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
   "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
+const GetApis = async () => {
+  try {
+    const response = await axios.get(
+      "https://64eaee50e51e1e82c576d9fa.mockapi.io/data/data"
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
 const Api = () => {
   const [items, setItems] = useState([]);
-  const [page, pagechange] = useState(0);
-  const [rowsPerPage, setRowPerPage] = useState(10);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  useEffect(() => {
+    GetApis().then((data) => {
+      setItems(data);
+    });
+  }, []);
 
   const handleChangePage = (event, newPage) => {
-    pagechange(newPage);
+    setPage(newPage);
   };
 
   const handleChangeRowsPerPage = (event) => {
-    setRowPerPage(+event.target.value);
-    pagechange(0);
+    setRowsPerPage(+event.target.value);
+    setPage(0);
   };
 
-  useEffect(() => {
-    setItems(data);
-  }, []);
+  // const deleteItem = (id) => {
+  //   // if (window.confirm("Are you sure?")) {
+  //   //   const newItems = items.filter((item) => item.id !== id);
+  //   //   setItems(newItems);
+  //   //   console.log("Deleted item with id: " + id);
+  //   // }
+  //   try {
+  //     const deleteSms = await fetch(`http://localhost:5000/sms/student/${id}`, {
+  //       method: "DELETE",
+  //       headers: {
+  //         jwt_token: localStorage.token
+  //       }
+  //     });
 
-  const deleteItem = (id) => {
-    if (window.confirm("Are you sure?")) {
-      const newItems = items.filter((item) => item.id !== id);
-      setItems(newItems);
-      console.log("đã xóa item " + id);
+  //     setSms(sms.filter(student => student.student_id !== id));
+
+  //   } catch (err) {
+  //     console.error(err.message);
+  //   }
+  // };
+
+  const deleteItem = async id => {
+    try {
+      const deleteItem = await fetch(`https://64eaee50e51e1e82c576d9fa.mockapi.io/data/data/${id}`, {
+        method: "DELETE",
+        headers: {
+          jwt_token: localStorage.token
+        }
+      });
+
+      setItems(items.filter(student => student.student_id !== id));
+      console.log("Deleted item with id: " + id);
+
+    } catch (err) {
+      console.error(err.message);
     }
-  };
+  }
 
-  const addItem = () => {
-    const newItem = { id: items.length + 1, name: `Item ${items.length + 1}` };
-    setItems([...items, newItem]);
-  };
 
   return (
     <div>
-      <h1> Get Api</h1>
+      <h1>Get Api</h1>
       <Container sx={{ mt: 10 }}>
         <TableContainer component={Paper} sx={{ maxHeight: 600 }}>
           <Table sx={{ minWidth: 650 }} stickyHeader aria-label="sticky table">
@@ -79,30 +117,30 @@ const Api = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {items.slice(0, rowsPerPage).map((item) => (
-                <StyledTableRow
-                  key={item.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <StyledTableCell component="th" scope="row">
-                    {" "}
-                    {item.id}{" "}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    {item.userId}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">{item.title}</StyledTableCell>
-                  <StyledTableCell align="center">
-                    <Button
-                      onClick={() => deleteItem(item.id)}
-                      variant="contained"
-                      color="success"
-                    >
-                      Delete
-                    </Button>
-                  </StyledTableCell>
-                </StyledTableRow>
-              ))}
+              {items
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((item, index) => (
+                  <StyledTableRow key={item.id}>
+                    <StyledTableCell component="th" scope="row">
+                      {item.id}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {item.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {item.stt}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      <Button
+                        onClick={() => deleteItem(item.id)}
+                        variant="contained"
+                        color="success"
+                      >
+                        Delete
+                      </Button>
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
             </TableBody>
           </Table>
         </TableContainer>
